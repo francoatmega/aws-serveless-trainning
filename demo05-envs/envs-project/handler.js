ea2ba539d890bb53781a747291b41556eea0dc2f
Jardel Matias <jardel.matias@itlean.com.br>
2022-08-12 22:32:27 -0300

    schedule implementation

--- demo05-envs/envs-project/handler.js
+++ demo05-envs/envs-project/handler.js
@@ -1,15 +1,37 @@
 'use strict';
+const envs = require('./serverless/envs-validation')
+const axios = require('axios')
+const cheerio = require('cheerio')
+const { randomUUID } = require('crypto')
+const aws = require('aws-sdk')
+const dynamoDB = new aws.DynamoDB.DocumentClient()
+class Handler {
+  static async main(args) {
 
-module.exports.scheduler = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: `Enviroment: ${process.env.NODE_ENV}`,
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
-};
+    console.log('at ', new Date().toISOString(), ' with args ', JSON.stringify(args, null, 2))
+
+    const { data } = await axios(envs.BASE_URL)
+
+    const $ = cheerio.load(data)
+    const [ commitMessage ] = $('#content').text().trim().split('\n')
+
+    const item = await dynamoDB.put({
+      TableName: envs.TABLE_NAME,
+      Item: {
+        commitMessage: commitMessage,
+        id: randomUUID(),
+        createdAt: new Date().toISOString()
+      }
+    }).promise()
+
+    console.log(item)
+
+    return {
+      statusCode: 200
+    }
+  }
+}
+
+module.exports = {
+  scheduler: Handler.main
+}
