1598ef245b8fc18354033f20f6a92303a1f2fe21
Jardel Matias <jardel.matias@itlean.com.br>
2022-09-03 18:59:42 -0300

    getting bucket name and object name in s3 trigger event

--- demo08-sqs-s3-streams/sqs-s3/src/s3.listener.js
+++ demo08-sqs-s3-streams/sqs-s3/src/s3.listener.js
@@ -3,15 +3,18 @@ require('../serverless/envs-validation')
 class Handler {
   static async main(args) {
     try {
+        const bucket = args.Records[0].s3.bucket.name
+        const object = args.Records[0].s3.object.key
+        console.log(`Processing object ${object} in ${bucket}...`)
         return {
-            statusCode: 200,
-            body: 'Hello'
-          }
+          statusCode: 200,
+          body: 'Hello'
+        }
     } catch (error) {
         return {
-            statusCode: 500,
-            body: 'Internal server error'
-          }
+          statusCode: 500,
+          body: 'Internal server error'
+        }
     }
   }
 }
