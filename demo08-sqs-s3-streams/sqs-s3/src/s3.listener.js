5fd38e5852704a1569b2c478a028099fee0592c2
Jardel Matias <jardel.matias@itlean.com.br>
2022-09-03 19:25:49 -0300

    adding stream

--- demo08-sqs-s3-streams/sqs-s3/src/s3.listener.js
+++ demo08-sqs-s3-streams/sqs-s3/src/s3.listener.js
@@ -1,16 +1,36 @@
 'use strict';
 require('../serverless/envs-validation')
+const s3 = require('../src/AWSFactory')('S3')
+const { Writable, pipeline } = require('stream')
 class Handler {
   static async main(args) {
     try {
-        const bucket = args.Records[0].s3.bucket.name
-        const object = args.Records[0].s3.object.key
-        console.log(`Processing object ${object} in ${bucket}...`)
+        const bucketName = args.Records[0].s3.bucket.name
+        const objectKey = args.Records[0].s3.object.key
+        console.log(`Processing object ${objectKey} in ${bucketName}...`)
+        s3.getObject({
+          Bucket: bucketName,
+          Key: objectKey
+        })
+        .createReadStream()
+        .on('data', data => {
+          console.log('Data ', data.toString())
+        })
+        .on('error', error => {
+          console.log('Error ', error)
+        })
+        .on('close', close => {
+          console.log('Close ', close)
+        })
+        .on('finish', _ => {
+          console.log('Finished! ')
+        })
         return {
           statusCode: 200,
           body: 'Hello'
         }
     } catch (error) {
+      console.log(error)
         return {
           statusCode: 500,
           body: 'Internal server error'
