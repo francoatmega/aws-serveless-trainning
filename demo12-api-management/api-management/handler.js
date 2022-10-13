c45693733ac9406850b008707ddff7e7f54f1bbe
Jardel Matias <jardel.matias@itlean.com.br>
2022-10-13 20:37:29 -0300

    add get plans route and get usage plans

--- demo12-api-management/api-management/handler.js
+++ demo12-api-management/api-management/handler.js
@@ -1,4 +1,6 @@
 'use strict';
+const aws = require('aws-sdk');
+const apiGateway = new aws.APIGateway();
 
 module.exports.hello = async (event) => {
   return {
@@ -8,6 +10,41 @@ module.exports.hello = async (event) => {
         message: 'Go Serverless v3.0! Your function executed successfully!',
         input: event,
       },
+      null,
+      2
+    ),
+  }; 
+};
+
+module.exports.getUsage = async (event) => {
+  const usage = await apiGateway.getUsagePlans().promise()
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      usage,
+      null,
+      2
+    ),
+  };
+};
+
+module.exports.getUsagePlans = async (event) => {
+  const {
+    from,
+    to,
+    planId,
+    apiKey
+  } = event.queryStringParameters
+  const usage = await apiGateway.getUsage({
+    startDate: from,
+    endDate: to,
+    usagePlanId: planId,
+    keyId: apiKey
+  }).promise()
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      usage,
       null,
       2
     ),
